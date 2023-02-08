137abce4b6ba58805eb2a637aefa825806abd044
Kulik Dzmitry <liamkukid@gmail.com>
2023-02-08 17:53:09 +0100

    GenericPipelineBehavior

---
+++ MediatRExample/GenericPipelineBehavior.cs
@@ -0,0 +1,13 @@
+namespace MediatRExample;
+
+public class GenericPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        Debug.WriteLine("-- Handling Request");
+        var response = await next();
+        Debug.WriteLine("-- Finished Request");
+        return response;
+    }
+}
--- MediatRExample/Program.cs
+++ MediatRExample/Program.cs
@@ -4,6 +4,7 @@ builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 
+builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(GenericPipelineBehavior<,>));
 var app = builder.Build();
 
 app.UseAuthorization();
