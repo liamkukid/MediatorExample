d9d0a5557df513f3f6b51bc15876d65c961e2bd2
Kulik Dzmitry <liamkukid@gmail.com>
2023-02-13 10:51:09 +0100

    TestRequestExceptionAction

---
+++ MediatRExample/RequestExceptionAction/TestBaseRequestExceptionAction.cs
@@ -0,0 +1,9 @@
+﻿namespace MediatRExample.RequestExceptionAction;
+
+public class TestBaseRequestExceptionAction : IRequestExceptionAction<TestBaseRequest, TestException>
+{
+    public async Task Execute(TestBaseRequest request, TestException exception, CancellationToken cancellationToken)
+    {
+        Debug.WriteLine($"Test base request exception action works: {exception.Message}");
+    }
+}
--- MediatRExample/RequestExceptionAction/TestRequestExceptionAction.cs
+++ MediatRExample/RequestExceptionAction/TestRequestExceptionAction.cs
@@ -1,10 +1,8 @@
-﻿using MediatR.Pipeline;
+﻿namespace MediatRExample.RequestExceptionAction;
 
-namespace MediatRExample.RequestExceptionAction;
-
-public class TestRequestExceptionAction : IRequestExceptionAction<TestBaseRequest, TestException>
+public class TestRequestExceptionAction : IRequestExceptionAction<TestRequest, TestException>
 {
-    public async Task Execute(TestBaseRequest request, TestException exception, CancellationToken cancellationToken)
+    public async Task Execute(TestRequest request, TestException exception, CancellationToken cancellationToken)
     {
         Debug.WriteLine($"Test request exception action works: {exception.Message}");
     }
